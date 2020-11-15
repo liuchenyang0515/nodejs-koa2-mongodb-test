04ffa29beb6aefc879722d34b9eabc27a1e96566
liuchenyang0515 <897418643@qq.com>
2020-11-15 11:07:05 +0800

    登录对接数据库

--- mongodb_practice/routes/users.js
+++ mongodb_practice/routes/users.js
@@ -1,4 +1,5 @@
 const router = require('koa-router')()
+const { User } = require('../db/model')
 
 router.prefix('/users') // 前缀
 
@@ -9,6 +10,33 @@ router.get('/', function (ctx, next) {
 router.get('/bar', function (ctx, next) {
   ctx.body = 'this is a users/bar response'
 })
+
+// 登录(对接数据库的)
+// 为了方便同域的测试，暂用get请求
+router.get('/login', async (ctx, next) => {
+  const { username, password } = ctx.query
+  // const { username, password } = ctx.request.body
+  const user = await User.findOne({
+    username,
+    password
+  })
+  if (user != null) {
+    // 登录成功
+    ctx.session.userInfo = user // 所有用户信息
+    ctx.body = {
+      errno: 0,
+      data: user
+    }
+    return 
+  }
+  // 登录失败，不用操作session
+
+  ctx.body = {
+    errno: -1,
+    message: '用户名或者密码错误'
+  }
+})
+
 
 // 模拟登录
 router.get('/login-mock', async (ctx, next) => {
